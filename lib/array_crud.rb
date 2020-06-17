24a61be5f4adb93ab6b5f40057ad7562d9cc6c69
james wu <jwu_nyc@yahoo.com>
2020-06-17 04:05:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  ["ben","james","stan","calvin"]
+  []
 end
 
 def create_an_array
-
+  ["ben","james","stan","calvin"]
 end
 
 def add_element_to_end_of_array(array, element)
